a507f6d3274a49086d1394b0c8d337179fd831a5
Riaan Kleinhans <61125752+Riaankl@users.noreply.github.com>
2023-02-28 13:43:16 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "null_resource" "sync_apt_repos" {
 
 resource "null_resource" "install_htop" {
   provisioner "local-exec" {
-    command = "sudo apt-get install -y htop"
+    command = <<-EOT 
+              sudo apt-get install -y htop
+              sudo apt install -y curl
+              sudo snap install -y helm --classic
+             EOT
   }
 }
