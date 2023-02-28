eb9ca5b5573c05b8679e2eced022957807dceb72
Riaan Kleinhans <61125752+Riaankl@users.noreply.github.com>
2023-02-28 22:54:31 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,6 @@ resource "null_resource" "install_docker" {
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
       sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
       sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-      #sudo usermod -aG docker ${USER}
       sudo usermod -aG docker riaan-ttb
     EOT
   }
