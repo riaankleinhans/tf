7eb2214036ae24752df685bcc0b2626b7fd2ee3d
Riaan Kleinhans <61125752+Riaankl@users.noreply.github.com>
2023-02-28 23:36:04 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,8 +76,7 @@ resource "null_resource" "install_kind" {
 resource "null_resource" "install_coder" {
   provisioner "local-exec" {
     command = <<-EOT
-      curl -fsSL https://coder.com/setup.sh | sudo bash -
-      sudo systemctl enable --now coder-server
+      Coder: sudo curl -fsSL https://coder.com/install.sh | sh
     EOT
   }
 }
