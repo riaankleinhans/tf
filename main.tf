4b63b646956a9a063b089fdf3df45549d9d2c1f3
Riaan Kleinhans <61125752+Riaankl@users.noreply.github.com>
2023-02-28 13:28:43 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+resource "null_resource" "sync_apt_repos" {
+  provisioner "local-exec" {
+    command = "sudo apt-get update && sudo apt-get upgrade"
+  }
+}
+
 resource "null_resource" "install_htop" {
   provisioner "local-exec" {
-    command = "sudo apt-get update && sudo apt-get install -y htop"
+    command = "sudo apt-get install -y htop"
   }
 }
