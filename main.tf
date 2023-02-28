deacc9f480da16d7c37c31e5ef265481b0b07c87
Riaan Kleinhans <61125752+Riaankl@users.noreply.github.com>
2023-02-28 22:48:09 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,3 +37,15 @@ resource "null_resource" "install_kubectl" {
     EOT
   }
 }
+
+resource "null_resource" "install_docker" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+      sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+      sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+      sudo usermod -aG docker ${USER}
+    EOT
+  }
+}
