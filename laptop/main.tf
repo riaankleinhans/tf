0be1e2df7c4c8936631dc103da0c840a2be436e4
Riaan Kleinhans <61125752+Riaankl@users.noreply.github.com>
2023-03-22 07:10:31 +1300

    Update main.tf

--- laptop/main.tf
+++ laptop/main.tf
@@ -144,21 +144,22 @@ resource "null_resource" "install_teamviewer" {
   depends_on = [null_resource.install_blender]
 }
 
-resource "null_resource" "install_golang" {
+resource "null_resource" "install_go" {
   provisioner "local-exec" {
-    command = <<EOT
-       GO_VERSION=1.20.2
-       curl -sSL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz | sudo tar --directory /usr/local --extract --ungzip
-EOT
+    command = <<-EOT
+      export GO_VERSION=1.17.5
+      curl -sSL https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
+      export PATH=$PATH:/usr/local/go/bin
+    EOT
   }
- depends_on = [null_resource.install_golang]
+ depends_on = [null_resource.install_blender]
 }
 
 resource "null_resource" "install_vagrant" {
   provisioner "local-exec" {
     command = "wget https://releases.hashicorp.com/vagrant/2.2.18/vagrant_2.2.18_x86_64.deb && sudo dpkg -i vagrant_2.2.18_x86_64.deb"
   }
- depends_on = [null_resource.install_teamviewer]
+ depends_on = [null_resource.install_go]
 }
 
 
@@ -166,7 +167,7 @@ resource "null_resource" "fix_broken_install" {
   provisioner "local-exec" {
     command = "sudo apt -y --fix-broken install"
   }
-  depends_on = [null_resource.install_teamviewer]
+  depends_on = [null_resource.install_go]
 }
 
 resource "null_resource" "autoremove" {
